82a7e436024580ca023aa30addd75a943e248264
Mario Noseda <mnoseda@bluewin.ch>
2019-07-23 10:47:42 +0200

    added examples from the book

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,84 @@
+// generic in struct definition
+
+// #[derive(Debug)]
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// fn main() {
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+
+//     println!("{:?}", integer);
+//     println!("{:?}", float);
+
+// }
+
+///////////////////////////////////////////////////////////
+
+// generic in struct definition (multiple types)
+
+// #[derive(Debug)]
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+
+// fn main() {
+//     let both_integer = Point { x: 5, y: 10 };
+//     let both_float = Point { x: 1.0, y: 4.0 };
+//     let integer_and_float = Point { x: 5, y: 4.0 };
+
+//     println!("{:?}", both_integer);
+//     println!("{:?}", both_float);
+//     println!("{:?}", integer_and_float);
+// }
+
+///////////////////////////////////////////////////////////
+
+// generics in method definition
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+// }
+
+// fn main() {
+//     let p = Point { x: 5, y: 10 };
+
+//     println!("p.x = {}", p.x());
+// }
+
+///////////////////////////////////////////////////////////
+
+// using different generics for the method definition and signature
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c'};
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
