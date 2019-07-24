e919f6a95aeaaeeeaccdf42429c36559bee30265
Mario Noseda <mnoseda@bluewin.ch>
2019-07-24 11:04:34 +0200

    added examples from the book

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,136 @@
-fn main() {
-    println!("Hello, world!");
+// try to create a dangling pointer (does not compile)
+
+// fn main() {
+//     let r;
+
+//     {
+//         let x = 5;
+//         r = &x;
+//     }
+
+//     println!("r: {}", r);
+// }
+
+//////////////////////////////////////////////////////////////
+
+// Compiler does not know which reference will be returned.
+// Therefore the lifetime of the returned reference cannot be 
+// determined. (does not compile)
+
+// fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// fn main() {
+//     let string1 = String::from("abcd");
+//     let string2 = "xyz";
+
+//     let result = longest(string1.as_str(), string2);
+//     println!("The longest string is {}", result);
+// }
+
+//////////////////////////////////////////////////////////////
+
+// lifetime annotations specify, that the returned reference gets
+// the same lifetime as the smaller lifetime of the input references
+
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// fn main() {
+//     let string1 = String::from("abcd");
+//     let string2 = "xyz";
+
+//     let result = longest(string1.as_str(), string2);
+//     println!("The longest string is {}", result);
+// }
+
+//////////////////////////////////////////////////////////////
+
+// lifetime annotations specify, that the returned reference gets
+// the same lifetime as the smaller lifetime of the input references
+// result has the same lifetime as the smaller of the input references-> ok
+
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// fn main() {
+//     let string1 = String::from("long string is long");
+
+//     {
+//         let string2 = String::from("xyz");
+//         let result = longest(string1.as_str(), string2.as_str());
+//         println!("The longest string is {}", result);
+//     }
+// }
+
+//////////////////////////////////////////////////////////////
+
+// result has a longer lifetime than the smaller lifetime of 
+// the input references -> does not compile 
+
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// fn main() {
+//     let string1 = String::from("long string is long");
+//     let result;
+//     {
+//         let string2 = String::from("xyz");
+//         result = longest(string1.as_str(), string2.as_str());
+//     }
+//     println!("The longest string is {}", result);
+// }
+
+//////////////////////////////////////////////////////////////
+
+// lifetime annotations in structs
+
+// struct ImportantExcerpt<'a> {
+//     part: &'a str,
+// }
+
+// fn main() {
+//     let novel = String::from("Call me Ishmael. Some years ago...");
+//     let first_sentence = novel.split('.')
+//         .next()
+//         .expect("Could not find a '.'");
+//     let i = ImportantExcerpt { part: first_sentence };
+// }
+
+//////////////////////////////////////////////////////////////
+
+// mixing lifetime annotations and generic type annotations
+
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
+
