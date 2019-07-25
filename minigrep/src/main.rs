432972caad4533a2e1f178329c845b1d9a291b44
Mario Noseda <mnoseda@bluewin.ch>
2019-07-25 21:19:41 +0200

    extracted the parse function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,8 +6,7 @@ fn main() {
 
     println!("args: {:?}",args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
@@ -15,4 +14,12 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
