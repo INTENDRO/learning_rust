4098a8d6fdbf8397fd43722f8202a9f89905bdc5
Mario Noseda <mnoseda@bluewin.ch>
2019-07-25 21:28:36 +0200

    using a struct to group config data

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,20 +6,25 @@ fn main() {
 
     println!("args: {:?}",args);
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
+struct Config {
+    query: String,
+    filename: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {query, filename}
 }
