95a19defe7450ff63f568d789e9d4e898508b562
Mario Noseda <mnoseda@bluewin.ch>
2019-07-26 10:07:43 +0200

    implemented search function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,15 +6,15 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    println!("args: {:?}",args);
+    // println!("args: {:?}",args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
